b227265cd283cda952d2641e87bad879f173563f
Foster Ogwudu <Foster.Ogwudu@yahoo.com>
2020-09-10 20:23:42 +0100

    Updates

---
+++ precache-manifest.c50312be93ec598887ced7cdaf65b87e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7aaa55f51bd40f6cdd36bb05c45d4b",
+    "url": "/Ogwudu-Family-Tree-App/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Ogwudu-Family-Tree-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8082429cb9991ec8fb27",
+    "url": "/Ogwudu-Family-Tree-App/static/css/main.67c8249c.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Ogwudu-Family-Tree-App/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ogwudu-Family-Tree-App/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8082429cb9991ec8fb27",
+    "url": "/Ogwudu-Family-Tree-App/static/js/main.b970d6af.chunk.js"
+  },
+  {
+    "revision": "9a09d3d9d133ab5725b2",
+    "url": "/Ogwudu-Family-Tree-App/static/js/runtime-main.aeb6fd67.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Ogwudu-Family-Tree-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
